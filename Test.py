0c99e593763b74e1c821a4e6a27386f13a21911d
RadaSTL <oarda.ert@gmail.com>
2020-10-19 02:43:31 +0300

    Tested scenarios

---
+++ Test.py
@@ -0,0 +1,8 @@
+testDic = {"mixed":[],"natural":[]}
+
+
+testDic["mixed"].append(["3","3","3"])
+testDic["mixed"].append(["1","2","Joker"])
+testDic["natural"].append(["4","5","6"])
+
+print(testDic)
--- mainGame.py
+++ mainGame.py
@@ -6,3 +6,13 @@ testGame.startGame(2)
 testGame.dealCard()
 
 print(testGame.getPile())
+
+card = testGame.getPile()[0]
+
+print(card)
+
+for card in testGame.getPlayerHand(0):
+    cardVal = testGame.getCardValue(card)%100
+    print(testGame.getCardValue(card))
+    print(cardVal)
+
